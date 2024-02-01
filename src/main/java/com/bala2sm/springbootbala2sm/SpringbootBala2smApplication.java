5f0dcc76f7cb2835e98b4e6c0f17b7fc5d92b2ba
Mohammed Al assad <mo_alass@live.concordia.ca>
2024-02-01 14:27:14 -0500

    setting up environment

---
+++ src/main/java/com/bala2sm/springbootbala2sm/SpringbootBala2smApplication.java
@@ -0,0 +1,13 @@
+package com.bala2sm.springbootbala2sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBala2smApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBala2smApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bala2sm/springbootbala2sm/SpringbootBala2smApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bala2sm.springbootbala2sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBala2smApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
