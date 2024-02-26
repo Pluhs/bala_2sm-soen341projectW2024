9cf35e82ab1502484d7faac3ae5700af3432c2cd
Domat <Domatalkhoury10@gmail.com>
2024-02-26 18:45:48 -0500

    Add User and Admin Classes

---
+++ src/main/java/com/bala2sm/springbootbala2sm/Admin.java
@@ -0,0 +1,38 @@
+package com.bala2sm.springbootbala2sm;
+
+public class Admin {
+
+    private String userName;
+    private String password;
+    private String role;
+
+    public Admin(String userName, String password, String role) {
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
