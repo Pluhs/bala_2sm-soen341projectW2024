38418d6542c978f86efdd5559378b512577884ad
elias-hannoun <eliashan858@gmail.com>
2024-03-10 23:07:56 -0400

    Final touches on the nav bar in both big and small screen

--- Main_React/my-app/src/Navbar/Navbar.js
+++ Main_React/my-app/src/Navbar/Navbar.js
@@ -8,6 +8,8 @@ class Navbar extends Component {
     state = {clicked: false};
     handleClick =()=> this.setState({clicked: !this.state.clicked})
 
+    handlePageOpened =()=> this.setState({clicked: false})
+
     render() {
         return (
             <nav className="NavbarItems">
@@ -19,7 +21,7 @@ class Navbar extends Component {
                     {MenuData.map((item, index) => {
                         return (
                             <li key={index}>
-                                <Link to={item.url} className={item.cName}>
+                                <Link to={item.url} className={item.cName} onClick={this.handlePageOpened}>
                                     <i className={item.icon}></i>{item.title}
                                 </Link>
                             </li>
